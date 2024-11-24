1f6a89a0b42694525a59b0c65759ec729ee6dca1
valer <valer@DESKTOP-QD94NVR>
2024-11-24 17:26:27 +0300

    Добавьте файлы проекта.

---
+++ Lab3_2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создание и заполнение двумерного массива 7x7
+        int[,] array = new int[7, 7];
+        Random random = new Random();
+
+        Console.WriteLine("Исходный массив:");
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                array[i, j] = random.Next(1, 10); // Заполнение массива случайными числами от 1 до 9
+                Console.Write(array[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        // Поворот массива на 90 градусов вправо
+        RotateArray(array);
+
+        Console.WriteLine("\nПовернутый массив на 90 градусов вправо:");
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void RotateArray(int[,] array)
+    {
+        int n = array.GetLength(0); // Получаем размерность массива
+
+        for (int layer = 0; layer < n / 2; layer++)
+        {
+            int first = layer;
+            int last = n - layer - 1;
+
+            for (int i = first; i < last; i++)
+            {
+                int offset = i - first;
+                // Сохраняем верхний элемент
+                int top = array[first, i];
+
+                // Перемещаем левый элемент в верхний
+                array[first, i] = array[last - offset, first];
+
+                // Перемещаем нижний элемент в левый
+                array[last - offset, first] = array[last, last - offset];
+
+                // Перемещаем правый элемент в нижний
+                array[last, last - offset] = array[i, last];
+
+                // Сохраняем верхний элемент в правый
+                array[i, last] = top;
+            }
+        }
+    }
+}
